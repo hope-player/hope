b8fa461cff4c1df269326367ff8ef7e028e2bfa8
Jan Suchomel <jansuchomel@gmx.com>
2016-07-04 23:01:17 +0200

    Implement collapsing in the LibraryView

--- src/components/LibraryView.jsx
+++ src/components/LibraryView.jsx
@@ -6,19 +6,46 @@ type LibraryViewProps = {
   library: NodeType,
 }
 
+type LibraryViewState = {
+  expanded: Set<string>,
+}
+
 export class LibraryView extends React.Component {
+  constructor(props : LibraryViewProps) {
+    super(props);
+    this.state = {
+      expanded: new Set('1'),
+    };
+  }
+
+  state: LibraryViewState
   props: LibraryViewProps
+
+  toggle(id : string) {
+    const nextExpanded = this.state.expanded;
+    if (this.state.expanded.has(id.toString())) {
+      nextExpanded.delete(id.toString());
+    } else {
+      nextExpanded.add(id.toString());
+    }
+    this.setState({ expanded: nextExpanded });
+  }
 
   renderNode(node : NodeType, key : string) : React.Element {
     return (
-      <li>
+      <li key={'li_' + key}><a onClick={this.toggle.bind(this, node.id)}> + </a>
         {
           node.component(`node_${key}`)
         }
-        <ul>
+        <ul key={'ul_' + key}>
         {
-          node.children.map((child : NodeType, i : number) : React.Component =>
-            this.renderNode(child, key + i))
+          node.children.map((child : NodeType, i : number) : React.Component => {
+            if (this.state.expanded.has(node.id)) {
+              return this.renderNode(child, key + i);
+            } else {
+              return null;
+            }
+          })
         }
         </ul>
       </li>
@@ -30,7 +57,7 @@ export class LibraryView extends React.Component {
     return (
       <div>
       {
-        this.renderNode(library, 1)
+        this.renderNode(library, '1')
       }
       </div>
     );
--- src/state/LibraryState.jsx
+++ src/state/LibraryState.jsx
@@ -3,36 +3,43 @@ import React from 'react';
 import { observable, computed } from 'mobx';
 
 export type NodeType = {
-  id: number,
+  id: string,
   type: string,
   children: Node[],
   component: (key : string) => React.Component;
 }
 
 export const LibraryState : NodeType = observable({
-  id: 1,
+  id: '1',
   type: 'root',
   children: [{
     id: 2,
     type: 'artist',
     children: [],
     @computed get component() : (key : string) => React.Element {
-      return (key : string) : React.Component => <p key={key}>artist 1</p>;
+      return (key : string) : React.Component => <span key={key}>artist 1</span>;
     },
   },
     {
-      id: 3,
+      id: '3',
       type: 'artist',
       children: [{
-        id: 4,
+        id: '4',
         type: 'album',
-        children: [],
+        children: [{
+          id: '5',
+          type: 'track',
+          children: [],
+          @computed get component() : (key : string) => React.Element {
+            return (key : string) : React.Element => <span key={key}>track</span>;
+          },
+        }],
         @computed get component() : (key : string) => React.Element {
-          return (key : string) : React.Element => <p key={key}>album</p>;
+          return (key : string) : React.Element => <span key={key}>album</span>;
         },
       }],
       @computed get component() : (key : string) => React.Component {
-        return (key : string) : React.Element => <p key={key}>artist 2</p>;
+        return (key : string) : React.Element => <span key={key}>artist 2</span>;
       },
     }],
   @computed get component() : (key : string) => React.Element {
