74f7e88f79510b4d0b35aa693a8e0d48bce03721
Jan Suchomel <jansuchomel@gmx.com>
2016-08-10 12:25:11 +0200

    Start work on the bottom player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 
 export default class Player extends React.Component {
   renderPlayer() {
-    const { pause, resume, playNext, playPrevious, currentTrack } = this.props;
+    const { pause, resume, playNext, playPrevious, currentTrack, playerState } = this.props;
     if (currentTrack) {
       return (
         <div>
-          <a onClick={pause}>pause</a>
-          <a onClick={resume}>resume</a>
-          <a onClick={playNext}>next</a>
-          <a onClick={playPrevious}>prev</a>
+          <a className="material-icons player-controls" onClick={playPrevious}>skip_previous</a>
+          {
+            (() => {
+              if (playerState === 'playing') {
+                return <a className="material-icons player-controls" onClick={pause}>pause_circle_outline</a>;
+              } else if (playerState === 'stopped' || playerState === 'paused') {
+                return <a className="material-icons player-controls" onClick={resume}>play_circle_outline</a>
+              }
+              return <a className="material-icons player-controls">help_outline</a>;
+            })()
+          }
+          <a className="material-icons player-controls" onClick={playNext}>skip_next</a>
           {currentTrack.get('name')}
         </div>
       );
@@ -21,7 +29,6 @@ export default class Player extends React.Component {
     const { playerState } = this.props;
     return (
       <div>
-        {playerState}
         {this.renderPlayer()}
       </div>
     );
